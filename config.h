17df28d742abbf405024dd8fd2ad02e868a7b26a
Pim Rutgers <Booli@users.noreply.github.com>
2021-08-24 17:40:39 +0200

    Added the MPPT 75_15 config from issue

--- config.h
+++ config.h
@@ -51,6 +51,55 @@
 	#define CHECKSUM 17
 #endif
 
+// MPPT 75 | 15
+#ifdef MPPT_75_15
+	const byte buffsize = 32;
+	const byte value_bytes = 33;
+	const byte label_bytes = 9;
+	const byte num_keywords = 19;
+
+	char keywords[num_keywords][label_bytes] = {
+	"PID",
+	"FW",
+	"SER#",
+	"V",
+	"I",
+	"VPV",
+	"PPV",
+	"CS",
+	"MPPT",
+	"ERR",
+	"LOAD",
+	"IL",
+	"H19",
+	"H20",
+	"H21",
+	"H22",
+	"H23",
+	"HSDS",
+	"Checksum"
+	};
+	#define PID 0
+	#define FW 1
+	#define SER 2	// Offically SER# but # does not play that well as macro
+	#define V 3     // ScV
+	#define I 4     // ScI
+	#define VPV 5   // PVV
+	#define PPV 6   // PVI = PVV / VPV
+	#define CS 7    // ScS
+	#define MPPT 8
+	#define ERR 9   // ScERR
+	#define LOAD 10  // SLs
+	#define IL 11   // SLI
+	#define H19 12
+	#define H20 13
+	#define H21 14
+	#define H22 15
+	#define H23 16
+	#define HSDS 17
+	#define CHECKSUM 18
+#endif
+
 // MPPT 100 | 20
 #ifdef MPPT_100_20
 
